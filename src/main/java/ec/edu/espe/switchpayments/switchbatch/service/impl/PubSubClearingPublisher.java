4c3d16b88d0b986ffda81d6e384acd2c82ad5cc3
alanherrera01 <avherrera3@espe.edu.ec>
2026-07-21 12:28:56 -0500

    Log PubSub clearing publish failures

--- src/main/java/ec/edu/espe/switchpayments/switchbatch/service/impl/PubSubClearingPublisher.java
+++ src/main/java/ec/edu/espe/switchpayments/switchbatch/service/impl/PubSubClearingPublisher.java
@@ -9,12 +9,16 @@ import com.google.pubsub.v1.PubsubMessage;
 import ec.edu.espe.switchpayments.switchbatch.config.FileReceptionProperties;
 import ec.edu.espe.switchpayments.switchbatch.dispatch.model.OffUsClearingMessage;
 import jakarta.annotation.PreDestroy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
 
 @Service
 public class PubSubClearingPublisher {
+
+    private static final Logger log = LoggerFactory.getLogger(PubSubClearingPublisher.class);
 
     private final ObjectMapper objectMapper;
     private final FileReceptionProperties properties;
@@ -38,6 +42,14 @@ public class PubSubClearingPublisher {
             ApiFuture<String> messageId = publisher.publish(pubsubMessage);
             messageId.get();
         } catch (Exception e) {
+            log.error("No se pudo publicar evento de clearing en Pub/Sub. projectId={}, topic={}, routingKey={}, batchId={}, transactionId={}, cause={}",
+                    properties.getPubsubProjectId(),
+                    properties.getPubsubClearingEventsTopic(),
+                    properties.getPubsubRoutingKeyClearingOutbound(),
+                    message.getBatchId(),
+                    message.getTransactionId(),
+                    e.getMessage(),
+                    e);
             throw new IllegalStateException("No se pudo publicar evento de clearing en Pub/Sub", e);
         }
     }
